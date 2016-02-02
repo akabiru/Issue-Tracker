fd1ca219b7b2f4c0ad330fd321cfa442f8e80244
Austin Kabiru <makabby@gmail.com>
2016-02-02 12:27:02 +0300

    Store password as password hash
    Use werkzeug  module to generate a password hash
    string with just two functions: generate_password_hash()
    and check_password_hash()

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class User(db.Model):
@@ -7,6 +8,22 @@ class User(db.Model):
     username = db.Column(db.String(10), unique=True)
     first_name = db.Column(db.String(20))
     last_name = db.Column(db.String(20))
+    password_hash = db.Column(db.String(128))
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute.')
+
+    @password.setter
+    def password(self, password):
+        '''Sets password to a hashed password
+        '''
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        '''Checks if password matches
+        '''
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return '<User %r>' % self.username
