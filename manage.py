57fd5326af4aadf1c3643fce575bd4e64510824b
Austin Kabiru <makabby@gmail.com>
2016-02-05 09:46:46 +0300

    env to production

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ from app.models import User, Role, Department, Issue
 from flask.ext.script import Manager, Shell
 from flask.ext.migrate import Migrate, MigrateCommand
 
-app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+app = create_app(os.getenv('FLASK_CONFIG') or 'production')
 manager = Manager(app)
 migrate = Migrate(app, db)
 
